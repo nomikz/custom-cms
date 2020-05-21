9f149044eeb875b0c39fcd4abc91f5fc107b3ab7
nomikz <1423qrwe>
2020-05-21 12:45:17 +0600

    commit

--- app/Event.php
+++ app/Event.php
@@ -20,23 +20,28 @@ class Event extends Model
         $events = Event::orderBy('date', 'desc')->get();
 
         $dates = $events->map(function ($event) {
-            return explode(' ', $event->date)[0];
+            return $event->getOnlyDate();
         })->unique();
 
-        $evensByDate = [];
+        $eventsByDate = [];
 
         foreach ($dates as $date) {
-            $eventsOfTheDate = $events
-                ->where('date', '>=', $date . ' 00:00:00')
-                ->where('date', '<', $date . ' 23:59:59')
-                ->sortByDesc('date');
+            $eventsOfTheDate = $events->filter(function($event) use ($date) {
+                return $event->getOnlyDate() === $date;
+            });
 
-            $evensByDate[] = [
+            $eventsByDate[] = [
                 'date' => $date,
                 'events' => EventResource::collection($eventsOfTheDate)
             ];
         }
-        return response()->json(['data' => $evensByDate, 'success' => true]);
+        return response()->json(['data' => $eventsByDate, 'success' => true]);
+    }
+
+
+    private function getOnlyDate() : string
+    {
+        return explode(' ', $this->date)[0];
     }
 
 }
